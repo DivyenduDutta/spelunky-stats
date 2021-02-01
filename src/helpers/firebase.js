8ad8f7488d849d4823c3f1b8e01c4be91d67d660
divyendu dutta <connect2divyendu@gmail.com>
2021-02-02 00:36:28 +0530

    welp

--- src/helpers/firebase.js
+++ src/helpers/firebase.js
@@ -1,59 +1,65 @@
 import firebase from "firebase/app";
-import { auth as firebaseAuthUi } from "firebaseui";
+import {auth as firebaseAuthUi} from 'firebaseui';
 import "firebase/auth";
-/*For local testing uncomment the below. Use local firebase project configs*/
-//import AuthConfig from "../../firebase.auth.json";
-
-//firebase.initializeApp(AuthConfig);
-/*Till here */
 
 let ui = null;
 let auth = null;
 let noCredentialsHelper = null;
 let authReady = null;
 
-fetch('/__/firebase/init.json').then(async response => {
-  firebase.initializeApp(await response.json());
+console.log(process.env.NODE_ENV);
 
-  /*Comment below lines*/
+if(process.env.NODE_ENV === 'production'){
+  /*For production */
+
+  fetch('/__/firebase/init.json').then(async response => {
+    firebase.initializeApp(await response.json());
+
+    ui = new firebaseAuthUi.AuthUI(firebase.auth());
+    auth = firebase.auth;
+    noCredentialsHelper = firebaseAuthUi.CredentialHelper.NONE;
+
+    authReady = () =>
+    new Promise((resolve, reject) => {
+      const unsubscribe = firebase.auth().onAuthStateChanged(
+        user => {
+          unsubscribe();
+          resolve(user);
+        },
+        error => {
+          unsubscribe();
+          reject(error);
+        }
+      );
+    });
+
+  });
+}
+
+if(process.env.NODE_ENV === 'development'){
+  /*For local/dev purposes */
+  const AuthConfig = require( "../../firebase.auth.json");
+  firebase.initializeApp(AuthConfig);
+  
+
   ui = new firebaseAuthUi.AuthUI(firebase.auth());
   auth = firebase.auth;
   noCredentialsHelper = firebaseAuthUi.CredentialHelper.NONE;
 
   authReady = () =>
-  new Promise((resolve, reject) => {
-    const unsubscribe = firebase.auth().onAuthStateChanged(
-      user => {
-        unsubscribe();
-        resolve(user);
-      },
-      error => {
-        unsubscribe();
-        reject(error);
-      }
-    );
-  });
-
-});
-
-/*Uncomment below lines */
-// ui = new firebaseAuthUi.AuthUI(firebase.auth());
-// auth = firebase.auth;
-// noCredentialsHelper = firebaseAuthUi.CredentialHelper.NONE;
-
-// authReady = () =>
-//   new Promise((resolve, reject) => {
-//     const unsubscribe = firebase.auth().onAuthStateChanged(
-//       user => {
-//         unsubscribe();
-//         resolve(user);
-//       },
-//       error => {
-//         unsubscribe();
-//         reject(error);
-//       }
-//     );
-//   });
+    new Promise((resolve, reject) => {
+      const unsubscribe = firebase.auth().onAuthStateChanged(
+        user => {
+          unsubscribe();
+          resolve(user);
+        },
+        error => {
+          unsubscribe();
+          reject(error);
+        }
+      );
+    });
+  }
 
 
 export { firebase, ui, authReady, auth, noCredentialsHelper };
